757151bcc4172c5c196149c19edef40bb00f651b
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2024-01-17 20:17:55 +0300

    webpack config decomposition

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,15 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { ProgressPlugin, WebpackPluginInstance } from 'webpack';
+
+import { BuildOptions } from './types/config';
+
+export function buildPlugins({ paths }: BuildOptions): WebpackPluginInstance[] {
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+
+    new ProgressPlugin(),
+  ];
+}
