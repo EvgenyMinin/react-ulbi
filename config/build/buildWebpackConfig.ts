bcaa6e1335e2d57264d34776ff31b12d96113a17
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2024-01-18 22:02:11 +0300

    dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,9 +6,11 @@ import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): Configuration {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
+
   return {
     mode,
     entry: paths.entry,
@@ -22,5 +24,7 @@ export function buildWebpackConfig(options: BuildOptions): Configuration {
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devServer: isDev ? buildDevServer(options) : undefined,
+    devtool: isDev ? 'inline-source-map' : false,
   };
 }
