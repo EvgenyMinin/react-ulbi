4819998e26df44c91a0a87014d37dc37c4a785b6
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2024-01-19 19:09:21 +0300

    react & scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,16 @@
 import { RuleSetRule } from 'webpack';
 
 export function buildLoaders(): RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+  };
+
   const typeScriptLoader = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   };
 
-  return [typeScriptLoader];
+  return [typeScriptLoader, cssLoader];
 }
