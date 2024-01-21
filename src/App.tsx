b54167319865ca0cacda39def756af31f5dd06dc
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2024-01-22 00:02:11 +0300

    routing & lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { Counter } from './components/Counter';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 import './index.scss';
+import { Suspense } from 'react';
 
 export const App = () => {
   return (
     <div className="app">
-      sdfsdfsdf
-      <Counter />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О Сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
