68ed4e1122baf755c7017433ba4aee1c757ea019
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2024-01-22 21:49:06 +0300

    styles & themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+
+import { useTheme } from './theme/useTheme';
 
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
-import './index.scss';
-import { Suspense } from 'react';
+import './styles/index.scss';
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О Сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactNode, useMemo, useState } from 'react';
+
+import { ETheme, LOCAL_STORAGE_THEME_KEY, ThemeContext } from './ThemeContext';
+
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as ETheme) || ETheme.LIGHT;
+
+type ThemeProviderProps = {
+  children?: ReactNode;
+};
+
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+  const [theme, setTheme] = useState<ETheme>(defaultTheme);
+
+  const defaultProps = useMemo(() => ({ theme, setTheme }), [theme]);
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
