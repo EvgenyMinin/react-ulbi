79797b20cb80d9c9c6d20274093c183fde854fce
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2024-01-24 20:23:02 +0300

    app router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import cn from 'classnames';
 
 import { useTheme } from 'app/providers/ThemeProvider';
 
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 import './styles/index.scss';
 
@@ -17,12 +15,7 @@ export const App = () => {
       <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О Сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
