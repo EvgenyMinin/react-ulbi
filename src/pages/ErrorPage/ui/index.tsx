e3ca6a769a89da1753b1aaec3e7072b7f65c209e
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2024-02-05 21:37:48 +0300

    error boundary

---
+++ src/pages/ErrorPage/ui/index.tsx
@@ -0,0 +1,19 @@
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui';
+
+import styles from './styles.module.scss';
+
+export const ErrorPage = () => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={styles.errorContainer}>
+      <p>{t('errorPage')}</p>
+      <Button onClick={reloadPage}>{t('refreshPage')}</Button>
+    </div>
+  );
+};
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,11 @@ import { useTranslation } from 'react-i18next';
 const MainPage = () => {
   const { t } = useTranslation('main');
 
-  return <div>{t('mainPage')}</div>;
+  return (
+    <div>
+      <span>{t('mainPage')}</span>
+    </div>
+  );
 };
 
 export default MainPage;
