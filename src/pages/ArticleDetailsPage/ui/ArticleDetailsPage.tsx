b0f1a788e1a893b6e2f35040b5b209e218b76fe3
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2025-10-14 19:46:18 +0300

    45 articles and publicPath webpack

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+const ArticleDetailsPage = () => {
+  const { t } = useTranslation('article');
+
+  return <div>{t('title')}</div>;
+};
+
+export default memo(ArticleDetailsPage);
