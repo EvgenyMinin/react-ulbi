783d4c10dd5595cc403bd0a1abcc4116f449ef7f
Evgeny <Evgeniy.Minin@leroymerlin.ru>
2025-10-16 19:53:37 +0300

    46 - react refresh plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,3 +1,4 @@
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {
@@ -28,6 +29,7 @@ export function buildPlugins({ paths, isDev, apiUrl }: BuildOptions): WebpackPlu
   ];
 
   if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
     plugins.push(new HotModuleReplacementPlugin());
     plugins.push(
       new BundleAnalyzerPlugin({
